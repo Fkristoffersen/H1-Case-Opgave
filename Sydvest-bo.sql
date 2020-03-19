595d4bac3db0ca27c04a4ca0b5b62544476d7ac6
Fkristoffersen <58325033+Fkristoffersen@users.noreply.github.com>
2020-03-19 21:58:40 +0100

    Update Sydvest-bo.sql

--- Sydvest-bo.sql
+++ Sydvest-bo.sql
@@ -137,7 +137,8 @@ ReservationsID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 SummerhusID INT NOT NULL FOREIGN KEY (SummerhusID) REFERENCES Sommerhuse(SummerhusID),
 KundeID INT NOT NULL FOREIGN KEY (KundeID) REFERENCES Kunde(KundeID),
 Lejeperiode varchar(60) NOT NULL,
-Sæson varchar(10) NOT NULL
+Sæson varchar(10) NOT NULL,
+Pris int NOT NULL
 )
 GO
 
